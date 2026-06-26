6960d67cc95bb17bd5eabbf996c6e6fac601d640
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:41 +0200

    new memory ranges

--- hardware/src/main/kotlin/components/devices/Console.kt
+++ hardware/src/main/kotlin/components/devices/Console.kt
@@ -5,15 +5,17 @@ import io.cuttlefish.components.*
 import kotlinx.coroutines.*
 
 class Console : MemoryManagement {
+    // jalr r0 r0 2 = print
+    // print what is in r1
     override suspend fun read(address: Short): Short {
         delay(Clock.DEVICE_CONSOLE_READ_TIME)
-        val char1 = (readlnOrNull()?.firstOrNull() ?: 0) as Char
-        return char1.code.toShort()
+        return withContext(Dispatchers.IO) {
+            return@withContext System.`in`.read()
+        }.toShort()
     }
 
     override suspend fun write(address: Short, value: Short) {
         delay(Clock.DEVICE_CONSOLE_WRITE_TIME)
 
-        TODO("Not yet implemented")
     }
 }
--- hardware/src/main/kotlin/e.kt
+++ hardware/src/main/kotlin/e.kt
@@ -1,14 +1,12 @@
 package io.cuttlefish
 
-import kotlin.experimental.*
-
 fun main() {
-    val imm: Short = 16_123
-    val lower6Mask: Short = 0x3F
-    val lowerPart = (imm and lower6Mask)
-    val upperPart = (imm.toInt() shr 6).toShort()
-    println("Lower = $lowerPart, Upper = $upperPart")
-    println((upperPart.toInt() shl 6).toShort() + lowerPart)
+    val first = System.`in`.read()
+    println("1 call: $first | ")
 
+    val second = System.`in`.read()
+    println("2 call: $second | ")
 
+    val third = System.`in`.read()
+    println("3 call: $third | ")
 }

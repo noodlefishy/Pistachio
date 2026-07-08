2e0a5dbed114229d77c46cbcb7faab237c4a1a1b
Needy Inky <needyinky@gmail.com>
2026-07-08 23:27:57 +0200

    🥕🥕🥕🥕🥕🥕🥕 It's a farm

--- hardware/src/main/kotlin/MemoryManagement.kt
+++ hardware/src/main/kotlin/MemoryManagement.kt
@@ -1,6 +1,9 @@
 package io.cuttlefish
 
-
+// 🥕🥕🥕🥕🥕🥕🥕 It's a farm
+// 🥕🥕🥕🥕🥕🥕🥕 It's a farm
+// 🥕🥕🥕🥕🥕🥕🥕 It's a farm
+// 🥕🥕🥕🥕🥕🥕🥕 It's a farm
 interface MemoryManagement {
     suspend fun read(address: Short): Short
     suspend fun write(address: Short, value: Short)

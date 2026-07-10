bda6ea78f471cae8e3246bf35ebb4f3871ea1c6e
Needy Inky <needyinky@gmail.com>
2026-07-10 23:56:51 +0200

    it is ugly, and slow, i did it because it looks cool.

--- hardware/src/main/kotlin/components/MemoryBus.kt
+++ hardware/src/main/kotlin/components/MemoryBus.kt
@@ -27,8 +27,8 @@ class MemoryBus(val ram: PhysicalMemory, val display: DisplayDevice) : MemoryMan
 }
 
 object MemoryMapRanges { // 64 KB
-    val vectorRange: IntRange   = 0x0000..0x003F // 0,0625  kb | 64w
-    val kernelRange: IntRange   = 0x0040..0x2FFF // 11,9375 kb | 12 224w
-    val userLandRange: IntRange = 0x3000..0xFDFF // 51,5    kb | 52 736w
-    val mmioRange: IntRange     = 0xFE00..0xFFFF // 0,5     kb | 512w
+    @JvmStatic internal val vectorRange: IntRange   by lazy { return@lazy 0x0000.rangeTo(other = 0x003F) } // 0,0625  kb | 64w
+    @JvmStatic internal val kernelRange: IntRange   by lazy { return@lazy 0x0040.rangeTo(other = 0x2FFF) } // 11,9375 kb | 12 224w
+    @JvmStatic internal val userLandRange: IntRange by lazy { return@lazy 0x3000.rangeTo(other = 0xFDFF) } // 51,5    kb | 52 736w
+    @JvmStatic internal val mmioRange: IntRange     by lazy { return@lazy 0xFE00.rangeTo(other = 0xFFFF) } // 0,5     kb | 512w
 }

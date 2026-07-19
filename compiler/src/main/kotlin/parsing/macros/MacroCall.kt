cd9c0105395a9b231cfb89531706cdb611cae2fd
Needy Inky <needyinky@gmail.com>
2026-07-19 16:46:33 +0200

    Fixed Call Macro + stack collision

--- compiler/src/main/kotlin/parsing/macros/MacroCall.kt
+++ compiler/src/main/kotlin/parsing/macros/MacroCall.kt
@@ -8,7 +8,7 @@ class MacroCall(val arg: Argument, line: Int, col: Int) : Statement(line, col) {
     override fun generate(context: ParserContext, address: Short): List<Instruction> {
         // Automatically translates to: movi r7, arg -> jalr r0, r7, 0
         val movi = MacroMovi(RegisterType.R7, arg, line, col).apply { this.scope = this@MacroCall.scope }
-        val jump = Instruction.Jalr(RegisterType.R0, RegisterType.R7, 0)
+        val jump = Instruction.Jalr(RegisterType.R7, RegisterType.R7, 0)
         return movi.generate(context, address) + jump
     }
 }

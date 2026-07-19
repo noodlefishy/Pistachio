2ebcddc3a55574ba6ae725fcbfc7ac54ccc5e8c6
Needy Inky <needyinky@gmail.com>
2026-07-19 08:26:12 +0200

    Opcode tokenisation, a bit risky

---
+++ compiler/src/main/kotlin/parsing/rules/OpcodeTokenRule.kt
@@ -0,0 +1,29 @@
+package io.cuttlefish.parsing.rules
+
+import io.cuttlefish.parsing.OpcodeToken
+import io.cuttlefish.parsing.TokenRule
+
+class OpcodeRule : TokenRule {
+    private val validOpcodes = setOf(
+        "add", "addi", "nand", "lui", "lw", "sw", "beq", "jalr",
+        "movi", "lli", "push", "pop", "call", "ret", "syscall", "halt", "nop",
+        ".fill", ".space"
+    )
+
+    // Note this matches any word, opcode or not, we check later if it is valid
+    private val regex = Regex("""^(?i)(\.?[a-z_]+)\b""")
+
+    override fun match(source: String, index: Int, line: Int, column: Int): TokenRule.MatchResult? {
+        val match = regex.find(source.substring(index)) ?: return null
+        if (match.range.first != 0) return null
+
+        val lexeme = match.groupValues[1].lowercase()
+
+        if (lexeme !in validOpcodes) return null
+
+        return TokenRule.MatchResult(
+            OpcodeToken(lexeme, line, column),
+            match.value.length
+        )
+    }
+}

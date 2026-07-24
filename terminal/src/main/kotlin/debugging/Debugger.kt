854acb35f5e3188f40f110eb45ac8f2a710cd1ae
Needy Inky <needyinky@gmail.com>
2026-07-25 00:25:08 +0200

    delta abstraction

--- terminal/src/main/kotlin/debugging/Debugger.kt
+++ terminal/src/main/kotlin/debugging/Debugger.kt
@@ -3,7 +3,6 @@ package io.cuttlefish.debug
 import io.cuttlefish.*
 import io.cuttlefish.backend.*
 import io.cuttlefish.components.*
-import io.cuttlefish.debugging.*
 
 class Debugger(val cpu: Cpu, val memory: MemoryBus) {
     val symbolMap: Map<String, UShort> = mapOf()
@@ -25,16 +24,34 @@ class Debugger(val cpu: Cpu, val memory: MemoryBus) {
         cpu.tick()
 
 
-
         val postRegisters = cpu.registers.registerData.copyOf()
         // TODO create log entry formats for history
 
 
     }
 
+
+    suspend fun run() {
+        while (!cpu.isHalted) {
+            executeStep()
+        }
+    }
+
+
+    private fun findRegisterDelta(pre: Array<Short>, post: Array<Short>): RegisterDelta? {
+        for (i in pre.indices) {
+            if (pre[i] != post[i]) return RegisterDelta(
+                registerType = RegisterType.entries[i],
+                oldValue = pre[i],
+                newValue = post[i]
+            )
+        }
+        return null
+    }
+
     private fun decipherLabel(label: UShort): String? {
         val labels: Map<UShort, String> =
-            symbolMap.map<String, UShort, Pair<UShort, String>> { it.value to it.key }.toMap()
+            symbolMap.map { it.value to it.key }.toMap()
         return labels[label]
     }
 

814d8580edaf4ca28a48c2cf71f72d235a2157b3
Needy Inky <needyinky@gmail.com>
2026-07-25 22:02:45 +0200

    formatting intergration

--- terminal/src/main/kotlin/debugging/Debugger.kt
+++ terminal/src/main/kotlin/debugging/Debugger.kt
@@ -3,7 +3,7 @@ package io.cuttlefish.debug
 import io.cuttlefish.*
 import io.cuttlefish.backend.*
 import io.cuttlefish.components.*
-import io.cuttlefish.debugging.formatInstruction
+import io.cuttlefish.debugging.*
 
 class Debugger(val cpu: Cpu, val memory: MemoryBus) {
     val symbolMap: Map<String, UShort> = mapOf()
@@ -26,14 +26,25 @@ class Debugger(val cpu: Cpu, val memory: MemoryBus) {
 
 
         val postRegisters = cpu.registers.registerData.copyOf()
-        // TODO create log entry formats for history
 
+        val delta = findRegisterDelta(preRegisters, postRegisters)
+        val traceString = formatTrace(prePc, decodedInstruction, delta)
+
+        if (history.size >= 50) history.removeFirst()
+        history.addFirst(traceString)
 
     }
 
 
-    suspend fun run() {
+    suspend fun runContinuously() {
         while (!cpu.isHalted) {
+            if (cpu.pc in breakPoints) {
+                println(
+                    "\n[DEBUG] Breakpoint hit at ${
+                        decipherLabel(cpu.pc) ?: "0x${cpu.pc.toString(16).padStart(4, '0')}"
+                    }"
+                )
+            }
             executeStep()
         }
     }
@@ -42,21 +53,18 @@ class Debugger(val cpu: Cpu, val memory: MemoryBus) {
     private fun findRegisterDelta(pre: Array<Short>, post: Array<Short>): RegisterDelta? {
         for (i in pre.indices) {
             if (pre[i] != post[i]) return RegisterDelta(
-                registerType = RegisterType.entries[i],
-                oldValue = pre[i],
-                newValue = post[i]
+                registerType = RegisterType.entries[i], oldValue = pre[i], newValue = post[i]
             )
         }
         return null
     }
 
-    fun formatTrace(pc: UShort, inst: Instruction, delta: RegisterDelta?) {
-        formatInstruction(pc, inst,delta)
+    fun formatTrace(pc: UShort, inst: Instruction, delta: RegisterDelta?): String {
+        return formatInstruction(pc, inst, delta)
     }
 
     private fun decipherLabel(label: UShort): String? {
-        val labels: Map<UShort, String> =
-            symbolMap.map { it.value to it.key }.toMap()
+        val labels: Map<UShort, String> = symbolMap.map { it.value to it.key }.toMap()
         return labels[label]
     }
 

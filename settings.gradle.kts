016873b83913780372f2921a945d3f51d3d6f278
Needy Inky <needyinky@gmail.com>
2026-08-28 14:10:03 +0200

    now has a wrapper! Plus good tests

--- settings.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-rootProject.name = "Pistachio"
-include("hardware")
-include("compiler")
-include("terminal")

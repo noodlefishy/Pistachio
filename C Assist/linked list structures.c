cb1ac0bfb444be9a78763de5e333187aeab4fd20
Needy Inky <needyinky@gmail.com>
2026-09-11 23:15:08 +0200

    less goog

--- C Assist/linked list structures.c
+++ C Assist/linked list structures.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
 
-typedef struct {
-	int data;
-	struct Node* next;
-} Node ;
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
 
-void add(Node** initReference, int dataNew) {
-	Node* new = (Node*)malloc(sizeof(Node))
-	new->data = dataNew
-	new->next = *initReference
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
 }
 
+// O(1) time
+void insertAtFront(Node** headRef, int data) {
+    Node* newNode = createNode(data);
+    newNode->next = *headRef;
+    *headRef = newNode;
+}
+
+void printList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+void freeList(Node* head) {
+    Node* tmp;
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
 
 int main() {
-	Node* initial = (Node*)malloc(sizeof(Node));
-    Node* second = (Node*)malloc(sizeof(Node));
-    Node* third = (Node*)malloc(sizeof(Node));
+    Node* head = NULL;
 
+    insertAtFront(&head, 30);
+    insertAtFront(&head, 20);
+    insertAtFront(&head, 10);
 
+    printf("Linked List Elements: ");
+    printList(head); // Output: 10 -> 20 -> 30 -> NULL
 
+    freeList(head);
+    head = NULL;
+
+    return 0;
 }
